c0edaef90a41aee2851a286d81df91490e52bb8c
Nimo Beeren <nimo@deity.io>
2019-12-07 00:10:56 +0100

    with forwardref

--- src/Item.js
+++ src/Item.js
@@ -1,7 +1,6 @@
 import React, { forwardRef } from "react";
 import "./Item.css";
 
-const ItemWithRef = ({ id }, ref) => (
+export const Item = forwardRef(({ id }, ref) => (
   <img ref={ref} src={`https://picsum.photos/id/${250 + id}/500`} />
-);
-export const Item = forwardRef(ItemWithRef);
+));
--- src/Rack.js
+++ src/Rack.js
@@ -10,21 +10,9 @@ export const Rack = ({ items }) => {
   return (
     <>
       <div className="rack">
-        {[...Array(items).keys()].map(item => {
-          return (
-            <div
-              key={item}
-              ref={item === 5 ? ref : console.log(item) && undefined}
-              style={{
-                minWidth: 280,
-                height: 280,
-                margin: 16,
-                background: "limegreen"
-              }}
-            />
-            // <Item key={item} id={item} ref={item === 5 ? ref : undefined} />
-          );
-        })}
+        {[...Array(items).keys()].map(item => (
+          <Item key={item} id={item} ref={item === 5 ? ref : undefined} />
+        ))}
       </div>
       <button onClick={handleScroll}>Scroll</button>
     </>

65cf860e42fb77f3b3b767ebe184ff0e894fc82c
Nimo Beeren <nimo@deity.io>
2019-12-07 23:30:44 +0100

    rough update handler

--- src/Rack.js
+++ src/Rack.js
@@ -1,19 +1,39 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import { Item } from "./Item";
 
 export const Rack = ({ items }) => {
-  const ref = useRef();
-  const handleScroll = () => {
-    ref.current.scrollIntoView({ behavior: "smooth" });
+  const [scrollPos, setScrollPos] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  // TODO: update on resize
+  const handleScroll = event => {
+    const container = event.currentTarget;
+
+    const pageWidth = container.getBoundingClientRect().width;
+    const totalWidth = container.scrollWidth;
+    console.log({ pageWidth, totalWidth });
+    const scrollPosNew = container.scrollLeft;
+    const currentPageNew = Math.floor(scrollPosNew / pageWidth) + 1;
+    const totalPagesNew = Math.ceil(totalWidth / pageWidth); // FIXME
+
+    setScrollPos(scrollPosNew);
+    setCurrentPage(currentPageNew);
+    setTotalPages(totalPagesNew);
   };
+
   return (
     <>
-      <div className="rack">
+      <div className="rack" onScroll={handleScroll}>
         {[...Array(items).keys()].map(item => (
-          <Item key={item} id={item} ref={item === 5 ? ref : undefined} />
+          <Item key={item} id={item} />
         ))}
       </div>
-      <button onClick={handleScroll}>Scroll</button>
+      <div>
+        <pre>scrollPos: {scrollPos}</pre>
+        <pre>currentPage: {currentPage}</pre>
+        <pre>totalPages: {totalPages}</pre>
+      </div>
     </>
   );
 };

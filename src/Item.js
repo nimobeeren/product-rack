a3122db4ef8a537ad0ede81d6b613b0b9e731924
Nimo Beeren <nimo@deity.io>
2019-12-08 00:55:36 +0100

    dynamic placeholders

--- src/Item.js
+++ src/Item.js
@@ -1,5 +1,8 @@
-import React, { forwardRef } from "react";
+import React from "react";
 
-export const Item = forwardRef(({ id = 0 }, ref) => (
-  <img ref={ref} src={`https://picsum.photos/id/${250 + id}/500`} />
-));
+export const Item = ({ id = 0, placeholder }) =>
+  placeholder ? (
+    <div className="item" />
+  ) : (
+    <img className="item" src={`https://picsum.photos/id/${250 + id}/500`} />
+  );
--- src/Rack.js
+++ src/Rack.js
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Item } from "./Item";
 
 export const Rack = ({ items }) => {
   const [scrollPos, setScrollPos] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
+  const [numPlaceholders, setNumPlaceholders] = useState(0);
+
+  const realItemsRef = useRef();
 
   // TODO: update on load
   // TODO: update on resize
@@ -13,17 +16,21 @@ export const Rack = ({ items }) => {
 
     const viewWidth = container.getBoundingClientRect().width;
     const totalWidth = container.scrollWidth;
-    const itemWidth = totalWidth / items;
-    const visibleItems = Math.floor(viewWidth / itemWidth); // FIXME not quite perfect
-    const pageWidth = visibleItems * itemWidth;
-    console.log({ pageWidth: viewWidth, totalWidth, itemWidth, visibleItems });
+    const itemWidth = totalWidth / (items + numPlaceholders); // FIXME not quite accurate
+    const itemsPerPage = Math.floor(viewWidth / itemWidth); // FIXME not quite accurate
+    const pageWidth = itemsPerPage * itemWidth;
+    console.log({ viewWidth, totalWidth, itemWidth, itemsPerPage });
     const scrollPosNew = container.scrollLeft;
     const currentPageNew = Math.floor(scrollPosNew / pageWidth) + 1;
-    const totalPagesNew = items / visibleItems;
+    const totalPagesNew = items / itemsPerPage;
+    // FIXME: when just on the edge of 1 more or less visible item, the number of placeholders
+    // affects the inaccuracy of itemWidth just enough to make it jitter
+    const numPlaceholdersNew = itemsPerPage - (items % itemsPerPage);
 
     setScrollPos(scrollPosNew);
     setCurrentPage(currentPageNew);
     setTotalPages(totalPagesNew);
+    setNumPlaceholders(numPlaceholdersNew);
   };
 
   return (
@@ -32,11 +39,15 @@ export const Rack = ({ items }) => {
         {[...Array(items).keys()].map(item => (
           <Item key={item} id={item} />
         ))}
+        {[...Array(numPlaceholders).keys()].map(item => (
+          <Item key={`placeholder${item}`} placeholder />
+        ))}
       </div>
       <div>
         <pre>scrollPos: {scrollPos}</pre>
         <pre>currentPage: {currentPage}</pre>
         <pre>totalPages: {totalPages}</pre>
+        <pre>numPlaceholders: {numPlaceholders}</pre>
       </div>
     </>
   );

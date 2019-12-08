9cf088f7e3c18c6a195d296aa39348d7907f33e8
Nimo Beeren <nimo@deity.io>
2019-12-08 11:52:57 +0100

    update on resize and mount

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import React from "react";
 import { Rack } from "./Rack";
 
-export const App = () => (
-  <div className="app">
-    <Rack items={8} />
-  </div>
-);
+export const App = () => <Rack items={8} />;
--- src/Rack.js
+++ src/Rack.js
@@ -1,39 +1,39 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Item } from "./Item";
 
 export const Rack = ({ items }) => {
-  const [scrollPos, setScrollPos] = useState(0);
+  console.log("render");
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
   const [numPlaceholders, setNumPlaceholders] = useState(0);
 
-  const realItemsRef = useRef();
+  const containerRef = useRef();
 
-  // TODO: update on load
-  // TODO: update on resize
-  const handleScroll = event => {
-    const container = event.currentTarget;
+  const update = () => {
+    const container = containerRef.current;
 
     const viewWidth = container.getBoundingClientRect().width;
     const totalWidth = container.scrollWidth;
-    const itemWidth = totalWidth / (items + numPlaceholders); // FIXME not quite accurate
-    const itemsPerPage = Math.floor(viewWidth / itemWidth); // FIXME not quite accurate
+    const gapWidth = parseFloat(window.getComputedStyle(container).columnGap);
+    const itemWidth = (totalWidth + gapWidth) / (items + numPlaceholders);
+    const itemsPerPage = Math.floor((viewWidth + gapWidth) / itemWidth);
     const pageWidth = itemsPerPage * itemWidth;
-    console.log({ viewWidth, totalWidth, itemWidth, itemsPerPage });
-    const scrollPosNew = container.scrollLeft;
-    const currentPageNew = Math.floor(scrollPosNew / pageWidth) + 1;
     const totalPagesNew = Math.ceil(items / itemsPerPage);
-    const numPlaceholdersNew = totalPagesNew * itemsPerPage - items;
 
-    setScrollPos(scrollPosNew);
-    setCurrentPage(currentPageNew);
+    console.log({ viewWidth, totalWidth, itemWidth, itemsPerPage });
+
+    setCurrentPage(Math.floor(container.scrollLeft / pageWidth) + 1);
     setTotalPages(totalPagesNew);
-    setNumPlaceholders(numPlaceholdersNew);
+    setNumPlaceholders(totalPagesNew * itemsPerPage - items);
   };
+
+  useEffect(update); // update on mount
+  const handleScroll = update;
+  window.onresize = update;
 
   return (
     <>
-      <div className="rack" onScroll={handleScroll}>
+      <div className="rack" ref={containerRef} onScroll={handleScroll}>
         {[...Array(items).keys()].map(item => (
           <Item key={item} id={item} />
         ))}
@@ -42,7 +42,6 @@ export const Rack = ({ items }) => {
         ))}
       </div>
       <div>
-        <pre>scrollPos: {scrollPos}</pre>
         <pre>currentPage: {currentPage}</pre>
         <pre>totalPages: {totalPages}</pre>
         <pre>numPlaceholders: {numPlaceholders}</pre>
